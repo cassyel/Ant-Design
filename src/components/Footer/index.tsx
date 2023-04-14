5edb096affffd23b5f52f055e56a9d94fa32e3b8
cassyel <cassyel1967@gmail.com>
2023-04-13 23:42:55 -0300

    feat(footer): create footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,16 @@
-import { SideMenuContainer } from './styles';
+import { Typography } from 'antd';
+import { FooterContainer } from './styles';
 
 export default function Footer() {
   return (
-    <SideMenuContainer>Footer</SideMenuContainer>
+    <FooterContainer>
+      <Typography.Text type='secondary'>
+        © {new Date().getFullYear()} Cassyel Oliveira,
+        Todos os direitos reservados.
+      </Typography.Text>
+      <Typography.Text type='secondary'>
+        Criado com Ant Design
+      </Typography.Text>
+    </FooterContainer>
   );
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
-export const SideMenuContainer = styled.footer`
-  height: 50px;
-  display: flex;
-  justify-content: space-evenly;
+export const FooterContainer = styled.footer`
   align-items: center;
+  justify-content: flex-end;
   border-top: 1px solid rgba(0, 0, 0, 15%);
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
 `;
