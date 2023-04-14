2b255e3ae78a6f65c78af5882f76698093c8cc6b
cassyel <cassyel1967@gmail.com>
2023-04-14 08:01:04 -0300

    feat(routes): create path routes

---
+++ src/components/Routes/index.ts
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/'
+  },
+  {
+    path: '/inventario'
+  },
+  {
+    path: '/pedidos'
+  },
+  {
+    path: '/clientes'
+  }
+]);
+
+export default router;
