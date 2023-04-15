ee89cb0d02bc8ceac4fa39bd29bc02cdccbe9478
cassyel <cassyel1967@gmail.com>
2023-04-15 20:17:41 -0300

    feat(dashboard): create dashboard page static

---
+++ src/components/DashboardCard/index.tsx
@@ -0,0 +1,46 @@
+import { AntdIconProps } from '@ant-design/icons/lib/components/AntdIcon';
+import { Card, Space, Statistic } from 'antd';
+import { ReactElement } from 'react';
+
+type iconProps = {
+  icon: ReactElement<AntdIconProps>;
+  color: string;
+  background: string;
+}
+
+interface IDashboardCardProps {
+  title: string;
+  value: number;
+  iconProps: iconProps
+}
+
+function IconCard({ iconProps }: { iconProps: iconProps }) {
+  return (
+    <i style={{
+      color: iconProps.color,
+      backgroundColor: iconProps.background,
+      borderRadius: 20,
+      fontSize: 24,
+      padding: 8,
+    }}
+    >
+      {iconProps.icon}
+    </i>
+  );
+}
+
+export default function DashboardCard({
+  title,
+  value,
+  iconProps,
+}: IDashboardCardProps) {
+
+  return (
+    <Card>
+      <Space direction='horizontal'>
+        <IconCard iconProps={iconProps} />
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
+}
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,59 @@
-import { Typography } from 'antd';
+import { Space, Typography } from 'antd';
+
+import { DashboardContainer } from './styles';
+import { DashboardCard } from '../../components';
+
+import {
+  DollarCircleOutlined,
+  ShoppingCartOutlined,
+  ShoppingOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+
 
 export default function Dashboard() {
   return (
-    <div>
+    <DashboardContainer>
       <Typography.Title level={4}>Dashboard</Typography.Title>
-    </div>
+      <Space direction='horizontal'>
+        <DashboardCard
+          title='Orders'
+          value={1234}
+          iconProps={{
+            icon: <ShoppingCartOutlined />,
+            background: 'rgba(0, 255, 0, 0.25)',
+            color: 'green',
+          }}
+        />
+        <DashboardCard
+          title='Inventory'
+          value={1234}
+          iconProps={{
+            icon: <ShoppingOutlined />,
+            background: 'rgba(0,0, 255, 0.25)',
+            color: 'blue',
+          }}
+        />
+        <DashboardCard
+          title='Customer'
+          value={1234}
+          iconProps={{
+            icon: <UserOutlined />,
+            background: 'rgba(0, 255, 255, 0.25)',
+            color: 'purple',
+          }}
+        />
+        <DashboardCard
+          title='Revenue'
+          value={1234}
+          iconProps={{
+            icon: <DollarCircleOutlined />,
+            background: 'rgba(255, 0, 0, 0.25)',
+            color: 'red',
+          }}
+        />
+      </Space>
+    </DashboardContainer>
   );
 }
